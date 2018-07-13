10e15fe56577fa046b7f81d3a160d5d6cb347201
Vapekreng <vapekreng@gmail.com>
2018-07-14 02:06:34 +0300

    Проверено - работает
    Изменились пути к переводам игры. Один язык - один файл. Обновление языка работает сходу

--- localization.py
+++ localization.py
@@ -1,7 +1,8 @@
 import os
 import settings
 
-LOCALIZATION_PATH = '\\DATA\\localization\\en-'
+LOCALIZATION_PATH = 'DATA\\localization\\en-'
+LOCALIZATION_PATH_LAST_SYMBOLS = '.txt'
 TRANSLATE_SEPARATOR = '='
 DEFAULT_LANGUAGE = 'en'
 
@@ -15,7 +16,8 @@ class Localization:
     def change_language(self, new_language):
         if self._new_language_is_correct(new_language):
             self.language = new_language
-            self._set_localization()
+            settings.current_settings.set_language(new_language)
+            self.localization = self._set_localization()
 
     def translate(self, text):
         if text in self.localization.keys():
@@ -26,7 +28,7 @@ class Localization:
     def _new_language_is_correct(new_language):
         if type(new_language) is not str:
             return False
-        if not os.path.isdir(LOCALIZATION_PATH + new_language):
+        if not os.path.isfile(LOCALIZATION_PATH + new_language + LOCALIZATION_PATH_LAST_SYMBOLS):
             return False
         return True
 
@@ -40,7 +42,7 @@ class Localization:
         return new_localization
 
     def _get_localization_path(self):
-        localization_path = LOCALIZATION_PATH + self.language
+        localization_path = LOCALIZATION_PATH + self.language + LOCALIZATION_PATH_LAST_SYMBOLS
         return localization_path
 
     def _get_localization_from_file(self, f):
@@ -54,8 +56,11 @@ class Localization:
     def _get_translate_from_line(line):
         original, splitted = '', ''
         if line.find(TRANSLATE_SEPARATOR) != -1:
-            line.replace('\n', '')
-            line.strip()
+            line = line.replace('\n', '')
+            line = line.strip()
             splitted_line = line.split(TRANSLATE_SEPARATOR)
-            original, translated = splitted_line[0], splitted_line[1]
-        return original, splitted
+            original, translated = splitted_line[0].strip(), splitted_line[1].strip()
+        return original, translated
+
+
+current_localization = Localization()
