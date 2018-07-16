16a4d719b8dd8395fd8c085212e2a411a2136a96
Vapekreng <vapekreng@gmail.com>
2018-07-16 22:16:51 +0300

    Согласовал названия функций с main_menu

--- start_game.py
+++ start_game.py
@@ -1,10 +1,11 @@
 from bearlibterminal import terminal
 from settings import current_settings
-from main_menu import MainMenu
+import main_menu
 
 terminal.open()
 font_name = current_settings.font_name
 font_size = current_settings.font_size
 terminal.set('font: %s, size=%d;' % (font_name, font_size))
-main_menu = MainMenu()
-main_menu.run_loop()
+main_menu.main_loop()
+terminal.close()
+
