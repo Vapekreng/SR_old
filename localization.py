b62b63293399e18db9f43ff6b51d0fa81b57696f
Vapekreng <vapekreng@gmail.com>
2018-07-16 13:04:07 +0300

    Переименовал переменные и методы

--- localization.py
+++ localization.py
@@ -11,13 +11,14 @@ class Localization:
 
     def __init__(self):
         self.language = settings.current_settings.language
-        self.localization = self._set_localization()
+        self.localization = {}
+        self._set()
 
     def change_language(self, new_language):
-        if self._new_language_is_correct(new_language):
+        if self._language_is_correct(new_language):
             self.language = new_language
             settings.current_settings.set_language(new_language)
-            self.localization = self._set_localization()
+            self._set()
 
     def translate(self, text):
         if text in self.localization.keys():
@@ -25,41 +26,41 @@ class Localization:
         return text
 
     @staticmethod
-    def _new_language_is_correct(new_language):
+    def _language_is_correct(new_language):
         if type(new_language) is not str:
             return False
         if not os.path.isfile(LOCALIZATION_PATH + new_language + LOCALIZATION_PATH_LAST_SYMBOLS):
             return False
         return True
 
-    def _set_localization(self):
-        localization_path = self._get_localization_path()
+    def _set(self):
+        path = self._get_path()
         new_localization = {}
-        if os.path.isfile(localization_path):
-            f = open(localization_path, 'r')
-            new_localization = self._get_localization_from_file(f)
+        if os.path.isfile(path):
+            f = open(path, 'r')
+            new_localization = self._pars_file(f)
             f.close()
-        return new_localization
+        self.localization = new_localization
 
-    def _get_localization_path(self):
-        localization_path = LOCALIZATION_PATH + self.language + LOCALIZATION_PATH_LAST_SYMBOLS
-        return localization_path
+    def _get_path(self):
+        path = LOCALIZATION_PATH + self.language + LOCALIZATION_PATH_LAST_SYMBOLS
+        return path
 
-    def _get_localization_from_file(self, f):
+    def _pars_file(self, f):
         new_localization = {}
         for line in f:
-            original, translated = self._get_translate_from_line(line)
+            original, translated = self._pars_line(line)
             new_localization[original] = translated
         return new_localization
 
     @staticmethod
-    def _get_translate_from_line(line):
-        original, splitted = '', ''
+    def _pars_line(line):
+        original, translated = '', ''
         if line.find(TRANSLATE_SEPARATOR) != -1:
             line = line.replace('\n', '')
             line = line.strip()
-            splitted_line = line.split(TRANSLATE_SEPARATOR)
-            original, translated = splitted_line[0].strip(), splitted_line[1].strip()
+            splitted = line.split(TRANSLATE_SEPARATOR)
+            original, translated = splitted[0].strip(), splitted[1].strip()
         return original, translated
 
 
